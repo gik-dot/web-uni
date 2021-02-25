331797fa28fb37c90138c2267a44dd9cde0d8586
gik-dot <nepetia3228@gmail.com>
2021-02-26 01:29:02 +0900

    update

--- code.php
+++ code.php
@@ -1,3 +1,32 @@
+ <!--Задание №1-->
 <blockquote>
-  <?phpecho s t r f t i m e('Сегодня%d-%m-%Y');?>
+  <?php echo strftime('Сегодня%d-%m-%Y');?>
 </blockquote>
+
+<!--Задание №2-->
+<blockquote>  
+<?php setlocale(LC_ALL, "russian");
+  $day = strftime('%d');
+  $mon = strftime('%B');
+  $year = strftime('%Y');?>
+</blockquote>
+
+<blockquote>
+  <?php echo 'Сегодня ', $day, ' число, ', $mon, ' месяц, ', $year, ' год.';?>
+  <?= strftime('%Y')?>
+</blockquote>
+
+<!--Задание №3-->
+<blockquote>
+  <?php echo "Сегодня $day число, $mon месяц, $year год.";?>
+  <?= strftime('%Y')?>
+</blockquote>
+
+<!--Задание №4-->
+<blockquote>
+  <?php define('COPYRIGHT', 'Веб-мастер');?>
+  <?php echo 'COPYRIGHT'?>
+</blockquote><!--Не уверен, что правильно-->
+
+<!--Задание №5-->
+<!--Честно, без понятия ¯\_(ツ)_/¯-->
