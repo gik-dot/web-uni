689d848f1640c2443921a6743ee0a284c9e8a9d3
gik-dot <nepetia3228@gmail.com>
2021-02-26 16:27:46 +0900

    new update

--- code.php
+++ code.php
@@ -1,11 +1,11 @@
  <!--Задание №1-->
-<blockquote>
-  <?php echo strftime('Сегодня%d-%m-%Y');?>
+ <blockquote>
+  <?php echo strftime('Сегодня %d-%m-%Y');?>
 </blockquote>
 
 <!--Задание №2-->
 <blockquote>  
-<?php setlocale(LC_ALL, "russian");
+<?php 
   $day = strftime('%d');
   $mon = strftime('%B');
   $year = strftime('%Y');?>
@@ -25,8 +25,17 @@
 <!--Задание №4-->
 <blockquote>
   <?php define('COPYRIGHT', 'Веб-мастер');?>
-  <?php echo 'COPYRIGHT'?>
+  <?php echo COPYRIGHT?>
 </blockquote><!--Не уверен, что правильно-->
 
 <!--Задание №5-->
-<!--Честно, без понятия ¯\_(ツ)_/¯-->
+<blockquote>
+  <?php 
+  $hour = (int)strftime('%H');
+  $welcome = '';
+  if ($hour < 6) $welcome = 'Добрый ночи';
+  elseif (($hour >= 6) && ($hour <= 12)) $welcome = 'Доброе утро';
+  elseif (($hour > 12) && ($hour <= 18)) $welcome = 'Добрый день';
+  elseif (($hour > 18) && ($hour <= 24)) $welcome = 'Доброй ночи';
+  echo $welcome?>
+</blockquote>
